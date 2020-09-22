6221dedfacc0e35f6d1c1d01af71c4699642ac0a
anujarora0502 <anujarora0502@gmail.com>
2020-09-22 22:56:42 +0530

    Updates

---
+++ precache-manifest.75f6c2de68f1ab7e9fbc99e035b08822.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3223c219837e473b634319b32866cd7",
+    "url": "/keeperApp/index.html"
+  },
+  {
+    "revision": "3470b0abc2913aafadbc",
+    "url": "/keeperApp/static/js/2.c89dd3b3.chunk.js"
+  },
+  {
+    "revision": "3e94ad0e0d2b81bc2bd8",
+    "url": "/keeperApp/static/js/main.e4424d06.chunk.js"
+  },
+  {
+    "revision": "6eb001f1e88b04caedff",
+    "url": "/keeperApp/static/js/runtime-main.b0e5c00f.js"
+  }
+]);
